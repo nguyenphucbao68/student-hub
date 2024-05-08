54b3d5096d187d731b7bc1500ab719a1361b3c43
anhzo12 <20127439clc1@gmail.com>
2024-05-08 14:44:03 +0700

    fix: setup zego

--- lib/screens/video_conference.dart
+++ lib/screens/video_conference.dart
@@ -10,9 +10,9 @@
 //   final String conferenceID;
 
 //   const VideoConferencePage({
-//     super.key,
+//     Key? key,
 //     required this.conferenceID,
-//   });
+//   }) : super(key: key);
 
 //   @override
 //   State<StatefulWidget> createState() => VideoConferencePageState();
@@ -25,22 +25,90 @@
 //   Widget build(BuildContext context) {
 //     print('da  toi');
 //     try {
-//       return Container();
+//       // return Container();
 
-//       // return SafeArea(
-//       //   child: ZegoUIKitPrebuiltVideoConference(
-//       //     appID: 1080864601,
-//       //     appSign:
-//       //         "9e3ba169e7b61d512d13b9d6fe63efceac69545a3aec980e446abfe38c8b4534",
-//       //     userID: localUserID,
-//       //     userName: 'user_$localUserID',
-//       //     conferenceID: "conferenceID",
-//       //     config: ZegoUIKitPrebuiltVideoConferenceConfig(),
-//       //   ),
-//       // );
+//       return SafeArea(
+//         child: ZegoUIKitPrebuiltVideoConference(
+//           appID: 1080864601,
+//           appSign:
+//               "9e3ba169e7b61d512d13b9d6fe63efceac69545a3aec980e446abfe38c8b4534",
+//           userID: localUserID,
+//           userName: 'user_$localUserID',
+//           conferenceID: "conferenceID",
+//           config: ZegoUIKitPrebuiltVideoConferenceConfig(),
+//         ),
+//       );
 //     } catch (e) {
 //       print('Error: $e');
 //       return Container();
 //     }
+//   }
+// }
+// Flutter imports:
+
+import 'package:flutter/material.dart';
+
+import 'package:zego_uikit_prebuilt_video_conference/zego_uikit_prebuilt_video_conference.dart';
+
+import 'dart:math' as math;
+
+final String localUserID = math.Random().nextInt(10000).toString();
+
+class VideoConferencePage extends StatefulWidget {
+  final String conferenceID;
+
+  const VideoConferencePage({
+    super.key,
+    required this.conferenceID,
+  });
+
+  @override
+  State<StatefulWidget> createState() => VideoConferencePageState();
+}
+
+class VideoConferencePageState extends State<VideoConferencePage> {
+  final controller = ZegoUIKitPrebuiltVideoConferenceController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ZegoUIKitPrebuiltVideoConference(
+          appID: 1247576097,
+          appSign:
+              '7d6266f0d5e6ce71461df7593e53b9a12c2b0e5faa342a061cc45251dc8c5009',
+          userID: localUserID,
+          userName: "user_$localUserID",
+          conferenceID: widget.conferenceID,
+          config: ZegoUIKitPrebuiltVideoConferenceConfig(
+            turnOnCameraWhenJoining: false,
+          )),
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:zego_uikit_prebuilt_video_conference/zego_uikit_prebuilt_video_conference.dart';
+
+// class VideoConferencePage extends StatelessWidget {
+//   final String conferenceID;
+
+//   const VideoConferencePage({
+//     Key? key,
+//     required this.conferenceID,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: ZegoUIKitPrebuiltVideoConference(
+//         appID: 1247576097,
+//         appSign:
+//             "7d6266f0d5e6ce71461df7593e53b9a12c2b0e5faa342a061cc45251dc8c5009",
+//         userID: 'user_id',
+//         userName: 'user_name',
+//         conferenceID: conferenceID,
+//         config: ZegoUIKitPrebuiltVideoConferenceConfig(),
+//       ),
+//     );
 //   }
 // }
